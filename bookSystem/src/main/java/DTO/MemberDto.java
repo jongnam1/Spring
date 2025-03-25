c698a7518939e74817a260bae3114c9a60e46157
jongnam1 <oklego2002@naver.com>
2025-03-25 13:41:22 +0900

    0325 Spring thymeleaf

---
+++ bookSystem/src/main/java/DTO/MemberDto.java
@@ -0,0 +1,14 @@
+package DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDto {
+	private int age;
+	private String name;
+	private String id;
+	private String tel;
+
+}
